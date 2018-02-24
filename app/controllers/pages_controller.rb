76481aefba06966d9b0ad954e16b89668790d20b
Capt-Obviouse <sixstringjesse@gmail.com>
2018-02-23 23:01:11 -0500

    Trying to get this to upload to Google

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,21 @@
 class PagesController < ApplicationController
   def about
+    @title = "About Us - Reaper Studios"
   end
 
   def home
+    @title = "Reaper Studios"
   end
 
   def contact
+      @title = "Contact Us - Reaper Studios"
   end
 
   def services
+      @title = "Services - Reaper Studios"
   end
 
   def portfolio
+      @title = "Portfolio - Reaper Studios"
   end
 end
