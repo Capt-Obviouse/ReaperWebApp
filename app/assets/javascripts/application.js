f29cc18c2230080a1fb2a07f3d8b512921765fc3
Capt-Obviouse <sixstringjesse@gmail.com>
2018-03-13 21:43:58 -0700

    re-design of the css. Initial setup of mobile css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,39 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function myFunction() {
+    var x = document.getElementById("nav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+/*
+window.onscroll = function () {
+  myFunction()
+};
+var x = 0;
+function myFunction()
+{
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0)
+  {
+    x = document.documentElement.scrollTop;
+    var imgHeight = 100;
+    if(100-x > 49)
+    {
+      document.getElementById("nav").style.height = 100 - x + 'px';
+      document.getElementById("nav").style.lineHeight = 100 - x + 'px';
+      document.getElementById("header-logo").style.height = 100 - x + 'px';
+    }
+  }
+  else
+  {
+      x = document.documentElement.scrollTop;
+      document.getElementById("nav").style.lineHeight = "100px";
+      document.getElementById("nav").style.height = "100px";
+      document.getElementById("header-logo").style.height = "100px";
+  }
+}
+*/
